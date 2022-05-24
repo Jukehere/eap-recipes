cd2d61afa5909a4a9f2768d5eb86c05a3f35a45f
JukeOtakuri <sofianopoulospanagiotis@gmail.com>
2022-05-24 11:39:14 +0300

    Myrec GUI

--- Myrec.py
+++ Myrec.py
@@ -1,77 +1,120 @@
 import sqlite3
+import tkinter
+from tkinter import messagebox
+from tkinter import ttk
 
-def myrecdelete(username):
+def myrecdelete(username,root):
+    def close(username):
+        delete.destroy()
+        myrecmenu(username)
+    def deleteacc(username):
+        connection = sqlite3.connect('RSD.db')
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM Users WHERE Username = ?",(username,))
+        connection.commit()
+        messagebox.showinfo(title="Success", message="Ο λογαριασμός σας διαγράφηκε.", parent = delete)
+        delete.destroy()
+        myrecmenu(username)
+    delete = tkinter.Tk()
+    delete.title('Σύστημα καταγραφής συνταγών μαγειρικής')
+    mainlabel = tkinter.Label(delete, text="Διαγραφή Λογαριασμού?", font=1)
+    mainlabel.grid(row=0, column=1)
+    signupb = tkinter.Button(root, text = "Ναι", command = lambda: deleteacc(username))
+    signupb.grid(row=2, column=2, padx=5, pady=5)
+    backb = tkinter.Button(root, text = "Όχι", command = lambda: close(username))
+    backb.grid(row=2, column=0, padx=5, pady=5)
+
+def myrecpass(username,root):
+    def close(username):
+        mrp.destroy()
+        myrecmenu(username)
+    def changepass(username):
+        connection = sqlite3.connect('RSD.db')
+        cursor = connection.cursor()
+        cursor.execute("UPDATE Users SET Password = ? WHERE Username = ?",(newpass,username,))
+        connection.commit()
+        messagebox.showinfo(title="Success", message="Επιτυχής αλλαγή.", parent = mrp)
+        mrp.destroy()
+        myrecmenu(username)
+    root.destroy()
+    mrp = tkinter.Tk()
+    mrp.title('Σύστημα καταγραφής συνταγών μαγειρικής')
+    mrpl = tkinter.Label(mrp, text="Αλλαγή Password", font=1)
+    mrpl.grid(row=0, column=1)
+    newpassl = tkinter.Label(mrp, text="Νέος κωδικός πρόσβασης:")
+    newpassl.grid(row=1, column=0)
+    newpass = tkinter.StringVar()
+    newpass = tkinter.Entry(mrp, width=30 , textvariable=newpass)
+    newpass.grid(row=1, column=1)
+    changeb = tkinter.Button(mrp, text = "Change Pass", command = lambda: changepass(username,newpass.get()))
+    changeb.grid(row=2, column=2, padx=5, pady=5)
+    backb = tkinter.Button(mrp, text = "Go Back", command = lambda: close(username))
+    backb.grid(row=2, column=0, padx=5, pady=5)
+
+def myrecrecipes(username,root):
+    def submitsel(select,username):
+        cursor.execute('SELECT * FROM Recipes WHERE RID = ? AND User = ?',(select,username,))
+        curcheck = cursor.fetchone()
+        if curcheck != None:
+            #send to showrec.py
+            show.destroy()
+        if curcheck == None:
+            messagebox.showerror("Error" , "Το RID δεν βρέθηκε, προσπάθησε ξανά." , parent = show)
+    def close(username):
+        show.destroy()
+        myrecmenu(username)
     connection = sqlite3.connect('RSD.db')
     cursor = connection.cursor()
-    check2 = 0
-    while check2 == 0:
-        impchoice = input("Νέος κωδικός πρόσβασης: (1 για διαγραφή, 2 για έξοδος)")
-        if impchoice == '2':
-            check2 = 1
-        if impchoice == '1':
-            cursor.execute("DELETE FROM Users WHERE Username = ?",(username,))
-            connection.commit()
-            print("\nΟ λογαριασμός σας διαγράφηκε.")
-            check2 = 1
-    connection.close()
-
-def myrecpass(username):
-    connection = sqlite3.connect('RSD.db')
-    cursor = connection.cursor()
-    check1 = 0
-    while check1 == 0:
-        newpass = input("Νέος κωδικός πρόσβασης: (- για έξοδος)")
-        if newpass == '-':
-            check1 = 1
-        if newpass != '-':
-            cursor.execute("UPDATE Users SET Password = ? WHERE Username = ?",(newpass,username,))
-            connection.commit()
-            print("Επιτυχής αλλαγή.")
-            check1 = 1
-    connection.close()
-
-def myrecrecipes(username):
-    connection = sqlite3.connect('RSD.db')
-    cursor = connection.cursor()
-    cursor.execute('SELECT (SELECT COUNT() FROM Steps) AS COUNT, * FROM Recipes WHERE User = ?',(username,))
+    cursor.execute('SELECT * FROM Recipes WHERE User = ?',(username,))
     curcheck = cursor.fetchall()
     if curcheck == []:
-        print("Δεν έχεις συνταγές στο σύστημα.")
+        messagebox.showerror("Error" , "Δεν υπάρχουν συνταγές στη συγκεκριμένη κατηγορία." , parent = root)
     if curcheck != []:
-        curcount = int(curcheck[0][0])
-        i = 1
-        for i in range(curcount-2):
-            print(i+1,". ",curcheck[i][1], " Βαθμολογία: ", curcheck[i][6])
-        check = 0
-        while check == 0:
-            choice = input("Επιλέξτε συνταγή: (- για έξοδος)")
-            if choice == "-":
-                check = 1
-            if choice != "-":
-                if int(choice) >= 1 and int(choice) <= i+1:
-                    #send to showrec.py
-                    pass
-                if int(choice) < 1 or int(choice) > i+1:
-                    print("Λάθος επιλογή, προσπάθησε ξανά.")
-    connection.close()
+        root.destroy()
+        show = tkinter.Tk()
+        show.title('Σύστημα καταγραφής συνταγών μαγειρικής')
+        showlabel = tkinter.Label(show, text=('Συνταγές:'), font=1)
+        showlabel.grid(row=0, column=1)
+        table = ttk.Treeview(show, columns=(1,2,3,4,5,6,7,8), show="headings", height=5)
+        table.grid(row=1, column=1, padx=10, pady=10)
+        table.heading(1, text="Name")
+        table.heading(2, text="User")
+        table.heading(3, text="RID")
+        table.heading(4, text="test")
+        table.heading(5, text="test")
+        table.heading(6, text="Rating")
+        table.heading(7, text="Rating")
+        table.heading(8, text="Rating")
+        table.column(1)
+        table.column(2, minwidth=0, width=0)
+        table.column(3)
+        table.column(4)
+        table.column(5, minwidth=0, width=0)
+        table.column(6)
+        table.column(7, minwidth=0, width=0)
+        table.column(8)
+        for i in curcheck:
+            table.insert('', 'end', values=i)
+        selectl = tkinter.Label(show, text="RID:")
+        selectl.grid(row=2, column=0)
+        select = tkinter.StringVar()
+        select = tkinter.Entry(show, width=30 , textvariable=select)
+        select.grid(row=2, column=1)
+        submitb = tkinter.Button(show, text = "Submit", command = lambda: submitsel(select.get(),username))
+        submitb.grid(row=3, column=2, padx=5, pady=5)
+        backb = tkinter.Button(show, text = "Go Back", command = lambda: close(username))
+        backb.grid(row=3, column=0, padx=5, pady=5)
 
 def myrecmenu(username):
-    print("\nMy Recipes\n")
-    check = 0
-    while check == 0:
-        print("1. Προβολή συνταγών μου")
-        print("2. Αλλαγή Password")
-        print("3. Διαγραφή λογαριασμού")
-        print("4. Έξοδος από το πρόγραμμα")
-        choice = input('\nΔιαλέξτε από τις παραπάνω επιλογές: ')
-        if choice == '1':
-            myrecrecipes(username)
-        if choice == '2':
-            myrecpass(username)
-        if choice == '3':
-            myrecdelete(username)
-            check = 1
-        if choice == '4': 
-            break
-        elif choice != '1' and choice != '2' and choice != '3' and choice != '4':
-            print("Άγνωστη επιλογή, προσπάθησε ξανά\n")
+    root = tkinter.Tk()
+    root.title('Σύστημα καταγραφής συνταγών μαγειρικής')
+    mainlabel = tkinter.Label(root, text=('My Recipes'), font=1)
+    mainlabel.grid(row=0, column=1)
+    myrecb = tkinter.Button(root, text="Προβολή συνταγών μου", padx=50, command= lambda: myrecrecipes(username,root))
+    myrecb.grid(row=1, column=1, padx=5, pady=5)
+    passwordb = tkinter.Button(root, text="Αλλαγή Password", padx=50, command= lambda: myrecpass(username,root))
+    passwordb.grid(row=2, column=1, padx=5, pady=5)
+    deleteb = tkinter.Button(root, text="Διαγραφή λογαριασμού", padx=50, command= lambda: myrecdelete(username,root))
+    deleteb.grid(row=3, column=1, padx=5, pady=5)
+    closeb = tkinter.Button(root, text="Έξοδος από το πρόγραμμα", padx=50, command= root.destroy)
+    closeb.grid(row=4, column=1, padx=5, pady=5)
